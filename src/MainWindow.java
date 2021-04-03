73350151c1513a43e5abbafcbe89fbef65b3c75d
rexx.magtar <rexx.magtar@gmail.com>
2021-04-04 01:23:00 +0300

    jar added

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,5 +1,3 @@
-import org.omg.CORBA.INTERNAL;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -44,59 +42,12 @@ public class MainWindow extends JFrame {
 
         JLabel timeLabel = new JLabel("Time:");
         timeLabel.setFont(labelFont);
-        timeLabel.setBounds(170, 10, 150, 40);
+        timeLabel.setBounds(10, 500, 150, 40);
         add(timeLabel);
-
-        JLabel x1Label = new JLabel("X1: ");
-        x1Label.setFont(labelFont);
-        x1Label.setBounds(10, 10, 50, 40);
-        add(x1Label);
-
-        x1Input.setBounds(70, 10, 100, 40);
-        x1Input.setFont(labelFont);
-        add(x1Input);
-
-        JLabel y1Label = new JLabel("Y1: ");
-        y1Label.setFont(labelFont);
-        y1Label.setBounds(10, 50, 50, 40);
-        add(y1Label);
-
-        y1Input.setBounds(70, 50, 100, 40);
-        y1Input.setFont(labelFont);
-        add(y1Input);
-
-
-        JLabel x2Label = new JLabel("X2: ");
-        x2Label.setFont(labelFont);
-        x2Label.setBounds(10, 110, 50, 40);
-        add(x2Label);
-
-        x2Input.setBounds(70, 110, 100, 40);
-        x2Input.setFont(labelFont);
-        add(x2Input);
-
-
-        JLabel y2Label = new JLabel("Y2: ");
-        y2Label.setFont(labelFont);
-        y2Label.setBounds(10, 150, 50, 40);
-        add(y2Label);
-
-        y2Input.setBounds(70, 150, 100, 40);
-        y2Input.setFont(labelFont);
-        add(y2Input);
-
-        JLabel radiusLabel = new JLabel("Rad.: ");
-        radiusLabel.setFont(labelFont);
-        radiusLabel.setBounds(10, 200, 60, 40);
-        add(radiusLabel);
-
-        radiusInput.setBounds(70, 200, 100, 40);
-        radiusInput.setFont(labelFont);
-        add(radiusInput);
 
 
         stepByStepButton = new JButton("step by step");
-        stepByStepButton.setBounds(10, 250, 160, 40);
+        stepByStepButton.setBounds(10, 10, 160, 40);
         stepByStepButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -116,7 +67,7 @@ public class MainWindow extends JFrame {
         add(stepByStepButton);
 
         DADButton = new JButton("DAD");
-        DADButton.setBounds(10, 300, 160, 40);
+        DADButton.setBounds(10, 60, 160, 40);
         DADButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -137,7 +88,7 @@ public class MainWindow extends JFrame {
         add(DADButton);
 
         brezenkhemButton = new JButton("Brezenkhem");
-        brezenkhemButton.setBounds(10, 350, 160, 40);
+        brezenkhemButton.setBounds(10, 110, 160, 40);
         brezenkhemButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -158,7 +109,7 @@ public class MainWindow extends JFrame {
         add(brezenkhemButton);
 
         brezenkhemCircleButton = new JButton("Bezenkhem circle");
-        brezenkhemCircleButton.setBounds(10, 400, 160, 40);
+        brezenkhemCircleButton.setBounds(10, 160, 160, 40);
         brezenkhemCircleButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -177,7 +128,7 @@ public class MainWindow extends JFrame {
         add(brezenkhemCircleButton);
 
         clearButton = new JButton("Clear");
-        clearButton.setBounds(10, 500, 200, 40);
+        clearButton.setBounds(10, 550, 200, 40);
         clearButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -186,6 +137,54 @@ public class MainWindow extends JFrame {
         });
 
         add(clearButton);
+
+        JLabel x1Label = new JLabel("X1: ");
+        x1Label.setFont(labelFont);
+        x1Label.setBounds(10, 250, 50, 40);
+        add(x1Label);
+
+        x1Input.setBounds(70, 250, 100, 40);
+        x1Input.setFont(labelFont);
+        add(x1Input);
+
+        JLabel y1Label = new JLabel("Y1: ");
+        y1Label.setFont(labelFont);
+        y1Label.setBounds(10, 300, 50, 40);
+        add(y1Label);
+
+        y1Input.setBounds(70, 300, 100, 40);
+        y1Input.setFont(labelFont);
+        add(y1Input);
+
+
+        JLabel x2Label = new JLabel("X2: ");
+        x2Label.setFont(labelFont);
+        x2Label.setBounds(10, 350, 50, 40);
+        add(x2Label);
+
+        x2Input.setBounds(70, 350, 100, 40);
+        x2Input.setFont(labelFont);
+        add(x2Input);
+
+
+        JLabel y2Label = new JLabel("Y2: ");
+        y2Label.setFont(labelFont);
+        y2Label.setBounds(10, 400, 50, 40);
+        add(y2Label);
+
+        y2Input.setBounds(70, 400, 100, 40);
+        y2Input.setFont(labelFont);
+        add(y2Input);
+
+        JLabel radiusLabel = new JLabel("Rad.: ");
+        radiusLabel.setFont(labelFont);
+        radiusLabel.setBounds(10, 450, 60, 40);
+        add(radiusLabel);
+
+        radiusInput.setBounds(70, 450, 100, 40);
+        radiusInput.setFont(labelFont);
+        add(radiusInput);
+
 
         setResizable(false);
     }
